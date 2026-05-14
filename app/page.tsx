d4742392e3be605abaeabff03e27144d6131097e
Ar-7q <arpitrai927@gmail.com>
2026-05-15 01:33:14 +0530

    initailzing the repository

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,131 @@
+"use client";
+import FeedCard from "@/components/FeedCard/page";
+
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+import { log } from "console";
 import Image from "next/image";
+import React, { useCallback } from "react";
+import { BiHash, BiHomeCircle, BiMoney, BiSolidUser, BiUserCheck, BiUserCircle, BiUserX } from "react-icons/bi";
+import { BsBellSlash, BsBookmarkCheckFill, BsBookmarkHeart, BsEnvelopeArrowUpFill, BsEnvelopeAt, BsEnvelopeDash, BsFillBellFill, BsFillEnvelopeDashFill } from "react-icons/bs";
+import { CgOptions } from "react-icons/cg";
+import { FaTwitter } from "react-icons/fa";
+import { FiAperture, FiCheckCircle } from "react-icons/fi";
+import { GiSpaceShuttle, GiSpaceSuit } from "react-icons/gi";
+import { RiSpaceShipLine } from "react-icons/ri";
+import { SiSpaceship } from "react-icons/si";
+
+interface TwitterSidebarButton {
+  title: string
+  icon: React.ReactNode
+}
+const sidebarMenuItems: TwitterSidebarButton[] = [
+  {
+    title: 'Home',
+    icon: <BiHomeCircle />
+  }, {
+    title: 'Explore',
+    icon: <BiHash />
+  }, {
+    title: 'Notifications',
+    icon: <BsFillBellFill />
+  }, {
+    title: 'Messages',
+    icon: <BsFillEnvelopeDashFill />
+  }, {
+    title: 'Bookmarks',
+    icon: <BsBookmarkHeart />
+  }, {
+    title: 'Tweet Blue',
+    icon: <BiMoney />
+  }, {
+    title: 'Profile',
+    icon: <BiSolidUser />
+  }, {
+    title: "More Options",
+    icon: <CgOptions />
+  }
+]
 
 export default function Home() {
+
+  const handleLoginwithGoogle = useCallback((cred: CredentialResponse) => { }, [])
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div>
+
+      <div className="grid grid-cols-12 min-h-screen w-full
+px-4 sm:px-6 md:px-8 lg:px-16 xl:px-32">
+
+        <div className="col-span-2 md:col-span-3 pt-1
+pl-2 sm:pl-4
+flex flex-col items-center lg:items-start
+min-w-[70px] sm:min-w-[80px] md:min-w-[220px]
+md:ml-2 lg:ml-10 xl:ml-20">
+          {/* twitter image from react icons */}
+          <div className="text-2xl h-fit w-fit hover:bg-gray-800 rounded-full p-4 cursor-pointer transition-all">
+            <FaTwitter />
+          </div>
+          <div className="mt-2 text-sm sm:text-base md:text-lg xl:text-xl pr-2 md:pr-4">
+            <ul>
+              {sidebarMenuItems.map((item) => (
+                <li className="flex justify-center lg:justify-start 
+items-center gap-2 md:gap-4
+hover:bg-gray-600 rounded-full
+px-2 md:px-3 py-2 md:py-3
+cursor-pointer w-fit mt-2 transition-all"
+                  key={item.title}>
+                  <span className="text-xl sm:text-2xl md:text-3xl">{item.icon}</span>
+                  <span className="hidden lg:block">
+                    {item.title}
+                  </span>
+                </li>
+              ))}
+            </ul>
+            <div className="mt-5 px-3">
+              <button className="group bg-[#1d9bf0] py-2 px-4 rounded-full w-full flex justify-center items-center overflow-hidden relative">
+
+                <span className="flex items-center gap-3 z-10">
+
+                  <FiCheckCircle className="text-white text-2xl" />
+
+                  <GiSpaceSuit
+                    className="text-gray-900 text-4xl drop-shadow-lg transition-transform duration-500 group-hover:-translate-y-1 group-hover:translate-x-2 group-hover:rotate-12 animate-pulse"
+                  />
+
+                </span>
+
+                <span className="absolute inset-0 bg-white/30 translate-x-[-100%] group-hover:translate-x-[100%] transition-transform duration-1000 skew-x-12"></span>
+
+              </button>
+            </div>
+          </div>
+
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="feed-container
+col-span-10 md:col-span-6 lg:col-span-5
+w-full
+border-l border-r border-gray-700">
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
         </div>
-      </main>
-    </div>
+
+        <div className="hidden lg:block lg:col-span-3 p-5">
+          <div className="p-5 bg-slate-700 rounded-lg">
+            <h1 className="my-2 text-2xl">
+              New User 🧰
+            </h1>
+
+
+
+            <GoogleLogin onSuccess={(cred) => console.log(cred)
+            } />
+          </div>
+        </div>
+      </div>
+    </div >
   );
 }
---
+++ components/FeedCard/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { generate } from "random-words";
+import { BiMessageRounded, BiUpload } from "react-icons/bi";
+import { FaRetweet } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa6";
+
+const FeedCard: React.FC = () => {
+    const [text, setText] = useState("");
+
+    useEffect(() => {
+        setText(generate({ exactly: 40, join: " " }));
+    }, []);
+
+    return (
+        <div>
+            <div className="border border-r-0 border-l-0 border-b-0 border-gray-600 p-3 sm:p-4 md:p-5 hover:bg-slate-900 transition-all cursor-pointer">
+                <div className="grid grid-cols-12 gap-2 md:gap-3">
+                    <div className="col-span-2 sm:col-span-1">
+                        <Image
+                            src='/tuyi.jpg'
+                            alt="user-image"
+                            className="rounded-full"
+                            height={50}
+                            width={50}
+                        />
+                    </div>
+
+                    <div className="col-span-10 sm:col-span-11">
+                        <h5 className="font-semibold text-sm sm:text-base">
+                            Meow Tiku
+                        </h5>
+
+                        <p className="text-xs sm:text-sm md:text-base break-words">
+                            {text}
+                        </p>
+
+                        <div className="flex justify-between mt-4 text-base sm:text-lg md:text-xl items-center p-1 sm:p-2 w-full sm:w-[90%]">
+                            <div>
+                                <BiMessageRounded />
+                            </div>
+
+                            <div>
+                                <FaRetweet />
+                            </div>
+
+                            <div>
+                                <FaHeart />
+                            </div>
+
+                            <div>
+                                <BiUpload />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FeedCard;
